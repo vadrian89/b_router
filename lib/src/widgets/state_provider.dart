ad907bdff0844f5a5cc4f50e7bea573f995f4576
Adrian Verban <verbanady@gmail.com>
2025-12-02 10:37:01 +0200

    Added an InheritedWidget used to pass the the state through context

---
+++ lib/src/widgets/state_provider.dart
@@ -0,0 +1,30 @@
+import 'package:b_router/src/bloc/b_router_cubit.dart';
+import 'package:flutter/widgets.dart';
+
+/// An inherited widget that provides access to the [BRouterState] through the build context.
+class BRouteStateProvider extends InheritedWidget {
+  /// The current state of the BRouter.
+  final BRouterState state;
+
+  const BRouteStateProvider({
+    super.key,
+    required this.state,
+    required super.child,
+  });
+
+  static BRouteStateProvider? maybeOf(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<BRouteStateProvider>();
+
+  static BRouteStateProvider of(BuildContext context) {
+    final state = maybeOf(context);
+    if (state == null) {
+      throw FlutterError(
+        'BRouteStateProvider.of() called with a context that does not contain a BRouteStateProvider.',
+      );
+    }
+    return state;
+  }
+
+  @override
+  bool updateShouldNotify(BRouteStateProvider oldWidget) => oldWidget.state != state;
+}
