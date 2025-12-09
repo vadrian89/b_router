d27fe525160274db63532543bfe36f14e3728f7c
Adrian Verban <verbanady@gmail.com>
2025-12-09 11:35:32 +0200

    Implemented BRouter

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -5,3 +5,4 @@ export 'src/widgets/not_found_screen.dart';
 export 'src/widgets/b_router_provider.dart';
 export 'src/widgets/b_router_listener.dart';
 export 'src/widgets/pop_result_listener.dart';
+export 'src/widgets/b_router.dart';
